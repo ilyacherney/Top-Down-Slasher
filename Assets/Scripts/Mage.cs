eb7ca31a2c54eaf411c4eb957d1d9e6c1018da97
ilyacherney <ilyacherney@yandex.ru>
2021-04-07 19:53:15 +0300

    Added mage

---
+++ Assets/Scripts/Mage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mage : Enemy
+{
+    private float spellRange = 7.0f;
+    private float distanceToPlayer;
+    private bool isCasting = false;
+
+    public override void Update()
+    {
+        base.Update();
+        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+    }
+    
+    public override void MoveTowardsPlayer()
+    {
+        // Chase player until it's close enough to cast a spell
+        if (!gameManagerScript.gameHasEnded && distanceToPlayer >= spellRange && !isCasting)
+        {
+            Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+            rb.AddForce(lookDirection * speed);
+        }
+        else if (!gameManagerScript.gameHasEnded && distanceToPlayer <= spellRange)
+        {
+            Spell();
+        }
+    }
+    IEnumerator CastingCountdownRoutine()
+    {
+        yield return new WaitForSeconds(1.4f);
+        isCasting = false;
+    }
+    public void Spell()
+    {
+        isCasting = true;
+        StartCoroutine(CastingCountdownRoutine());
+
+        Debug.Log("Pew pew its magic");
+    }
+}
